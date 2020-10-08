2875033bebec49c22e8ed75835b581f4c1697d01
Flight15 <omrigez9@gmail.com>
2020-10-08 20:00:23 +0300

    hi

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,20 @@
 import React from "react";
+import { render } from "react-dom";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
+import {ReactMic} from 'react-mic';
 
 const HomeScreen = ({navigation}) => {
+  render()
   return <View>
       <Text style={styles.text}>SaveMe</Text>
       <Button
       onPress={() => navigation.navigate('Components')}
       title="Go To Settings Screen" />
+      <Button
+      title="Start Recording"
+      />
   </View>
-};
+}
 
 const styles = StyleSheet.create({
   text: {
